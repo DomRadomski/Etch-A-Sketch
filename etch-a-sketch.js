580000653c166e14cb2363b49a036daedbf07e9a
DomRadomski <domradomski123@gmail.com>
2025-10-18 17:00:31 +0100

    children now added aswell as css to make children pop out as the mouse hovers over them

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,14 +3,22 @@ const container = document.getElementById("container");
 function createDivs(num) 
 {
 
-    for (let i = 0; i < num; i++)
+    for (let x = 0; x < num; x++)
     {
         const parent = document.createElement("div");
         parent.classList.add("parent");
+
+        for (let y = 0; y < num; y++)
+        {
+            const child = document.createElement("div");
+            child.classList.add("child");
+
+            parent.appendChild(child);
+        }
 
         container.appendChild(parent);  
 
     }
 }
 
-createDivs(4);
+createDivs(6);
